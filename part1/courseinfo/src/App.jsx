da6224840573cdf7ca1f3f927bb286aa5e928a6e
Tereza Holm <holm.tereza@gmail.com>
2025-04-10 09:03:21 +0200

    Finished courseinfo step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,13 @@
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part}: {props.exercises} exercises
+      </p>
+    </div>
+  );
+};
+
 const Header = (props) => {
   return (
     <div>
@@ -9,15 +19,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
