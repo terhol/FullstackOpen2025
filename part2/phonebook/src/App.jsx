4a04f1e1d073c00a9513f86fb828135c38415d8c
Tereza Holm <holm.tereza@gmail.com>
2025-05-04 13:00:18 +0200

    Finished phonebook part 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import AddPerson from "./components/AddPerson";
 import Numbers from "./components/Numbers";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filteredName, setFilteredName] = useState("");
@@ -19,6 +15,12 @@ const App = () => {
       : persons.filter((person) =>
           person.name.toUpperCase().includes(filteredName.toUpperCase())
         );
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   return (
     <div>
