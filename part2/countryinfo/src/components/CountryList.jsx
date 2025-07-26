a4396c5139f52d6f2b8ede7152a13fb145ed2848
Tereza Holm <holm.tereza@gmail.com>
2025-07-26 23:24:11 +0200

    Countryinfo - step 2 finished

--- part2/countryinfo/src/components/CountryList.jsx
+++ part2/countryinfo/src/components/CountryList.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import Country from "./Country";
 
 const CountryList = ({ countries, filteredName }) => {
-  const filteredCountries = countries.filter((country) =>
-    country.toUpperCase().includes(filteredName.toUpperCase())
-  );
+  const [filteredCountries, setFilteredCountries] = useState(countries);
+
+  useEffect(() => {
+    setFilteredCountries(
+      countries.filter(
+        (country) => country.toUpperCase().includes(filteredName.toUpperCase())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filteredName]);
 
   if (filteredCountries.length > 10) {
     return <div> Too many matches, specify another filter.</div>;
   }
+
+  const handleClickOnShow = (country) => {
+    setFilteredCountries([country]);
+  };
 
   return (
     <div>
@@ -16,7 +29,12 @@ const CountryList = ({ countries, filteredName }) => {
       ) : (
         <ul>
           {filteredCountries.map((country) => {
-            return <li key={country}>{country}</li>;
+            return (
+              <li key={country}>
+                {country}{" "}
+                <button onClick={() => handleClickOnShow(country)}>Show</button>
+              </li>
+            );
           })}
         </ul>
       )}
